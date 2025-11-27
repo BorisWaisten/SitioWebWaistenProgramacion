dd1fc2ca4d8bad1250f2de4923ae2f2863873ba3
BorisWaisten <104634915+BorisWaisten@users.noreply.github.com>
2025-11-26 23:49:58 -0300

    instalacion de analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata as NextMetadata } from "next";
 import { Urbanist } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 import "./globals.css";
 import 'swiper/css';
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className={urbanist.className}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
